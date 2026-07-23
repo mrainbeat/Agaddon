a828e62bc38406bb5756b589dd5f25d6e10dfd9f
minjun <mrainbeat@gmail.com>
2026-07-23 11:41:31 +0900

    [Feat] 마이페이지 구현

--- project/vite.config.js
+++ project/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"; // 1. 패키지 import 추가
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // 2. 테일윈드 플러그인 추가
+  ],
   server: {
     port: 3000,
   },
